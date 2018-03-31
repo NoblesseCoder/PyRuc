b1d14e9d4bd46c649e794c26c6b6783c5052d79f
ASMITA RAO <asmitarao777@gmail.com>
2018-03-31 11:39:55 +0530

    Add files via upload

--- ruby_test.rb
+++ ruby_test.rb
@@ -1,17 +1,16 @@
 puts "Hello, Ruby!"
 
-# This is a comment
-putsabc
 
 j=11
 for i in 0..5
-	j=1
-	if i > 2 then
+	m=15
+	j = 23
+	if m > 2 then
 		abs=12
 		break
   	end
 	k=12
-	puts"Value of local variable is #{i}"
+	str = "hey"
 end
 y=2
 
